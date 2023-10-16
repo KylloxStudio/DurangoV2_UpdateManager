3c6b204edf8fd13b381cf84af289c75511e2e992
Kyllox <kyllox4804@gmail.com>
2023-10-17 03:03:10 +0900

    Update

--- DurangoV2_UpdateManager/CheckUpdate.cs
+++ DurangoV2_UpdateManager/CheckUpdate.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Diagnostics;
-using HtmlAgilityPack;
 
 namespace CheckUpdate
 {
@@ -15,43 +14,32 @@ namespace CheckUpdate
 
 		static void Main(string[] args)
 		{
+			if (args != null && args.Length > 0 && args[0] == "update")
+			{
+				DownloadUpdate.Program.Update();
+				return;
+			}
+
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-			HtmlWeb web = new HtmlWeb();
-			HtmlDocument document = web.Load("https://github.com/KylloxStudio/Durango_V2/tags");
-			HtmlNode h2Node = document.DocumentNode.SelectSingleNode("//h2[@class='f4 d-inline']");
-			if (h2Node != null)
+			WebClient webClient = new WebClient();
+			string address = "https://raw.githubusercontent.com/KylloxStudio/Durango_V2/main/DurangoV2_Data/version.txt";
+			LatestVersion = double.Parse(webClient.DownloadString(address));
+
+			string path = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName).FullName + "\\version.txt";
+
+			if (new FileInfo(path).Exists)
 			{
-				HtmlNode aNode = h2Node.SelectSingleNode("a");
-				if (aNode != null)
+				string txt = File.ReadAllText(path);
+				if (double.TryParse(txt, out double ver))
 				{
-					string[] version = aNode.InnerText.Split(new char[]
-					{
-						'v'
-					});
-					LatestVersion = double.Parse(version[1]);
-
-					string path = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName).FullName + "\\version.txt";
-
-					if (new FileInfo(path).Exists)
-					{
-						string txt = File.ReadAllText(path);
-						if (double.TryParse(txt, out double ver))
-							CurrentVersion = ver;
-					}
-					else
-					{
-						Error = true;
-						Console.WriteLine("Error: Cannot Found 'version.txt' File.");
-						foreach (Process process in Process.GetProcesses())
-						{
-							if (process.ProcessName.StartsWith("DurangoV2_UpdateManager"))
-							{
-								process.Kill();
-							}
-						}
-					}
+					CurrentVersion = ver;
 				}
+			}
+			else
+			{
+				Error = true;
+				Console.WriteLine("Error: Cannot Found 'version.txt' File.");
 			}
 
 			if (!Error)
@@ -63,22 +51,6 @@ namespace CheckUpdate
 				Console.WriteLine("Current Version: " + string.Format("{0:0.0}", CurrentVersion));
 				Console.WriteLine("Latest Version: " + string.Format("{0:0.0}", LatestVersion));
 				Console.WriteLine();
-			}
-
-			if (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) == Directory.GetCurrentDirectory())
-			{
-				if (FoundUpdate)
-					DownloadUpdate.Program.Update();
-			}
-			else
-            {
-				string path2 = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\FoundUpdate.txt";
-				if (new FileInfo(path2).Exists)
-				{
-					string txt = File.ReadAllText(path2);
-					if (FoundUpdate && txt.IndexOf("True") != -1)
-						DownloadUpdate.Program.Update();
-				}
 			}
 		}
 	}
--- DurangoV2_UpdateManager/DownloadUpdate.cs
+++ DurangoV2_UpdateManager/DownloadUpdate.cs
@@ -2,39 +2,24 @@
 using System.IO;
 using System.Net;
 using System.Threading;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
-using HtmlAgilityPack;
 
 namespace DownloadUpdate
 {
 	class Program
 	{
-		[DllImport("kernel32.dll")]
-		private static extern IntPtr GetConsoleWindow();
-
-		[DllImport("user32.dll")]
-		private static extern IntPtr GetSystemMenu(IntPtr windowHandle, bool revert);
-
-		[DllImport("user32.dll")]
-		private static extern bool EnableMenuItem(IntPtr menuHandle, uint menuItemID, uint enabled);
-
-		private const uint SC_CLOSE = 0xf060;
-		private const uint MF_ENABLED = 0x00000000;
-		private const uint MF_GRAYED = 0x00000001;
+		static string DirectoryPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).FullName).FullName).FullName;
 
 		public static void Update()
 		{
-			Console.Title = "DownloadUpdate_DurangoV2";
-			IntPtr consoleWindowHandle = GetConsoleWindow();
-
+			Console.Title = "DurangoV2_UpdateManager - Updating...";
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
 			string downloadPath = string.Empty;
 			string localPath = string.Empty;
-			string pastFileName = string.Empty;
-			string fileName = string.Empty;
-			bool foundUpdate = false;
+			string downloadedFileName = string.Empty;
+			string downloadingFileName = string.Empty;
 			bool isDownloading = false;
 			int fileLength = 0;
 
@@ -43,10 +28,10 @@ namespace DownloadUpdate
 			{
 				if (isDownloading)
 				{
-					if (pastFileName != fileName)
+					if (downloadedFileName != downloadingFileName)
 					{
-						pastFileName = fileName;
-						Console.Write("\"{0}\": Downloading... ", fileName);
+						downloadedFileName = downloadingFileName;
+						Console.Write("\"{0}\": Downloading... ", downloadingFileName);
 					}
 				}
 			};
@@ -58,86 +43,36 @@ namespace DownloadUpdate
 					for (int i = 0; i <= 100; i++)
 					{
 						progress.Report((double)i / 100);
-						Thread.Sleep(fileLength / 1000000);
+						Thread.Sleep(fileLength / 8263019);
 					}
 				}
 				Console.WriteLine("Completed.");
-				Thread.Sleep(50);
 				isDownloading = false;
 			};
 
-			Console.Title = "DownloadUpdate_DurangoV2 - Updating...";
-			SetCloseButtonEnabled(consoleWindowHandle, false);
-
-			string address = "https://durangomanager.000webhostapp.com/client/isUpdateEtc.txt";
-			string txt = new StreamReader(webClient.OpenRead(address)).ReadToEnd();
-			bool isUpdateEtc = bool.Parse(txt);
-			int downloadCount = 3;
-			string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).FullName).FullName).FullName;
-			if (isUpdateEtc)
-				downloadCount = 6;
-
-			for (int i = 0; i < downloadCount; i++)
+			string[] downloadPaths = new StreamReader(webClient.OpenRead("https://durango-database.vercel.app/update/UpdateFilePaths.txt")).ReadToEnd().Split('\n');
+			string[] localPaths = new StreamReader(webClient.OpenRead("https://durango-database.vercel.app/update/UpdateFileLocalPaths.txt")).ReadToEnd().Split('\n');
+			for (int i = 0; i < downloadPaths.Length; i++)
 			{
-				while (isDownloading)
-				{
-				}
-				string name = string.Empty;
-				if (i == 0)
-				{
-					downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/Managed/Assembly-CSharp.dll?raw=true";
-					localPath = path + "\\Managed\\Assembly-CSharp.dll";
-					name = "Assembly-CSharp.dll";
-				}
-				else if (i == 1)
-				{
-					downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/resources.assets?raw=true";
-					localPath = path + "\\resources.assets";
-					name = "resources.assets";
-				}
-				else if (i == 2)
-				{
-					downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/version.txt?raw=true";
-					localPath = path + "\\version.txt";
-					name = "version.txt";
-				}
-
-				if (downloadCount > 3)
-				{
-					if (i == 3)
-					{
-						downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/StreamingAssets/Movie/PC/title.mp4?raw=true";
-						localPath = path + "\\StreamingAssets\\Movie\\PC\\title.mp4";
-						name = "title.mp4";
-					}
-					else if (i == 4)
-					{
-						downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/StreamingAssets/Movie/PC/warping.mp4?raw=true";
-						localPath = path + "\\StreamingAssets\\Movie\\PC\\warping.mp4";
-						name = "warping.mp4";
-					}
-					else if (i == 5)
-					{
-						downloadPath = "https://github.com/KylloxStudio/Durango_V2/blob/main/DurangoV2_Data/StreamingAssets/Audio/GeneratedSoundBanks/Windows/title.bnk?raw=true";
-						localPath = path + "\\StreamingAssets\\Audio\\GeneratedSoundBanks\\Windows\\title.bnk";
-						name = "title.bnk";
-					}
-				}
-
 				try
 				{
+					while (isDownloading)
+					{
+						Thread.Sleep(100);
+					}
+					downloadPath = downloadPaths[i];
+					localPath = DirectoryPath + localPaths[i];
 					HttpWebRequest request = (HttpWebRequest)WebRequest.Create(downloadPath);
 					HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 					if (response.StatusCode == HttpStatusCode.OK)
 					{
 						while (webClient.IsBusy)
-						{
-						}
-						while (isDownloading)
-						{
+                        {
+							Thread.Sleep(100);
 						}
 						webClient.DownloadFileAsync(new Uri(downloadPath), localPath);
-						fileName = name;
+						string[] split = localPaths[i].Split('\\');
+						downloadingFileName = split[split.Length - 1];
 						isDownloading = true;
 					}
 					response.Close();
@@ -152,78 +87,33 @@ namespace DownloadUpdate
 				}
 			}
 
-			Console.Title = "DownloadUpdate_DurangoV2";
-			SetCloseButtonEnabled(consoleWindowHandle, true);
+			while (isDownloading)
+			{
+				Thread.Sleep(100);
+			}
 
 			Console.WriteLine();
 			Console.WriteLine();
 			Console.WriteLine("All Files Update Completed.");
+			Console.WriteLine("Press any key to restart the game.");
 
-			bool isOpenedUrl = false;
-			if (foundUpdate)
-			{
-				Console.WriteLine();
-				Console.WriteLine("자동 업데이트 다운로더의 업데이트가 발견되었습니다. 다운로드 페이지로 이동하시겠습니까? (Y / N)\n(다운로드 후 게임 폴더 내의 [Durango_V2_Data//Programs] 폴더에 압축을 풀어주세요.");
-				Console.WriteLine("An update of the automatic update downloader has been found. Do you want to go to the download page? (Y / N)\n(After manual download, and unzip into the [DurangoV2_Data//Programs] folder.)");
-				while (true)
-				{
-					string input = Console.ReadLine();
-					if (input == "Y" || input == "y")
-					{
-						Process.Start("https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/KylloxStudio/Durango_V2/tree/main/DurangoV2_Data/Programs/UpdateManager");
-						isOpenedUrl = true;
-						break;
-					}
-					else if (input == "N" || input == "n")
-					{
-						break;
-					}
-				}
-			}
-			else
-            {
-				Console.WriteLine("Press any key to restart the game.");
-				Console.ReadKey();
-			}
-
-			string path2 = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\FoundUpdate.txt";
-			File.Delete(path2);
-			foreach (Process process in Process.GetProcesses())
-			{
-				if (process.ProcessName.IndexOf("UpdateManager") == -1 && process.ProcessName.IndexOf("DurangoV2") != -1)
-				{
-					process.Kill();
-					break;
-				}
-				else
-                {
-					Console.WriteLine("Cannot Found exe file.");
-					break;
-                }
-			}
-
-			if (!isOpenedUrl)
+			Console.ReadKey();
+			try
 			{
 				foreach (Process process in Process.GetProcesses())
 				{
-					if (process.ProcessName.IndexOf("UpdateManager") == -1 && process.ProcessName.IndexOf("DurangoV2") != -1)
+					if (process.ProcessName.IndexOf("UpdateManager") == -1 && process.ProcessName.IndexOf("Durango") != -1)
 					{
-						process.Start();
-						break;
-					}
-					else
-					{
-						Console.WriteLine("Cannot Found exe file.");
+						process.Kill();
+						Process.Start(process.ProcessName);
 						break;
 					}
 				}
 			}
-		}
-
-		private static void SetCloseButtonEnabled(IntPtr windowHandle, bool enabled)
-		{
-			IntPtr systemMenuHandle = GetSystemMenu(windowHandle, false);
-			EnableMenuItem(systemMenuHandle, SC_CLOSE, MF_ENABLED | (enabled ? MF_ENABLED : MF_GRAYED));
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
 		}
 	}
 }
